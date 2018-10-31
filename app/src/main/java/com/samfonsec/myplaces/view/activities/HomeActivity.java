11349212abe486fb9f86ef4e3afa88c491084143
samfonsec <samfonsec@gmail.com>
2018-10-31 08:37:10 -0300

    create recycler view and adapters

--- app/src/main/java/com/samfonsec/myplaces/view/activities/HomeActivity.java
+++ app/src/main/java/com/samfonsec/myplaces/view/activities/HomeActivity.java
@@ -1,34 +1,44 @@
 package com.samfonsec.myplaces.view.activities;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
 
 import com.samfonsec.myplaces.R;
+import com.samfonsec.myplaces.databinding.ActHomeBinding;
 import com.samfonsec.myplaces.model.LocationListEntity;
 import com.samfonsec.myplaces.utils.DialogUtils;
+import com.samfonsec.myplaces.view.adapters.LocationAdapter;
 import com.samfonsec.myplaces.viewmodel.HomeViewModel;
 
 
 public class HomeActivity extends AppCompatActivity {
+    ActHomeBinding binding;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.act_home);
+        binding = DataBindingUtil.setContentView(this, R.layout.act_home);
+
 
         HomeViewModel viewModel = new HomeViewModel();
 
         // start to observe
-        viewModel.onLocationsResponse().observe(this, this::onDetailResponse);
+        viewModel.onLocationsResponse().observe(this, this::onLocationsResponse);
         viewModel.onError().observe(this, this::onError);
 
         DialogUtils.showProgressDialog(this);
         viewModel.getLocations();
     }
 
-    private void onDetailResponse(LocationListEntity locationsList) {
+    private void onLocationsResponse(LocationListEntity locationsList) {
         DialogUtils.hideProgressDialog();
+
+        LocationAdapter locationAdapter = new LocationAdapter(locationsList.getLocations());
+        binding.rvLocations.setAdapter(locationAdapter);
+        binding.rvLocations.setLayoutManager(new GridLayoutManager(this, 2));
 
     }
 
--- app/src/main/java/com/samfonsec/myplaces/view/adapters/LocationAdapter.java
+++ app/src/main/java/com/samfonsec/myplaces/view/adapters/LocationAdapter.java
@@ -1,4 +1,48 @@
 package com.samfonsec.myplaces.view.adapters;
 
-public class LocationAdapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.samfonsec.myplaces.R;
+import com.samfonsec.myplaces.model.LocationEntity;
+
+import java.util.List;
+
+public class LocationAdapter extends RecyclerView.Adapter<LocationAdapter.ItemHolder> {
+    private List<LocationEntity> locations;
+
+    public LocationAdapter(List<LocationEntity> locations) {
+        this.locations = locations;
+    }
+
+    @NonNull
+    @Override
+    public ItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.location_item_adapter, parent, false);
+        return new ItemHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemHolder holder, int position) {
+        LocationEntity location = this.locations.get(position);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return locations.size();
+    }
+
+    class ItemHolder extends RecyclerView.ViewHolder {
+        View view;
+
+        ItemHolder(View view) {
+            super(view);
+            this.view = view;
+        }
+    }
 }
